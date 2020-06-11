88860cb76d2505140cdacde6e0f747e06b3c6cca
zakariachamakh <53184374+zakariachamakh@users.noreply.github.com>
2020-06-11 12:56:22 +0100

    add populer favourite Page

--- lib/screens/home_tabs/favourite.dart
+++ lib/screens/home_tabs/favourite.dart
@@ -1,6 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
 
 class Favourite extends StatefulWidget {
   @override
@@ -10,8 +9,100 @@ class Favourite extends StatefulWidget {
 class _FavouriteState extends State<Favourite> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
+    return ListView.builder(itemBuilder: (context, index) {
+      return Card(
+        child: Container(
+          padding: EdgeInsets.only(top: 12, left: 4),
+          child: Column(
+            children: <Widget>[
+              _headerRowCard(),
+              _drawArticleRow(),
+            ],
+          ),
+        ),
+      );
+    },itemCount: 20,);
+  }
+
+  Widget _headerRowCard() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(right: 8),
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: ExactAssetImage('assets/images/bg.png'),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                Text(
+                  'zakaria chamakh',
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('15min'),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      'lifestyle',
+                      style: TextStyle(color: Colors.teal),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+        IconButton(icon: Icon(Icons.bookmark), onPressed: () {})
+      ],
+    );
+  }
+
+  Widget _drawArticleRow() {
+    return Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 14, left: 12, bottom: 32, right: 12),
+          width: 112,
+          height: 112,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: ExactAssetImage('assets/images/bg.png'),
+                fit: BoxFit.cover),
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              Text(
+                "title of the single article of the news app tha is just test one",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                maxLines: 2,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                "description of the single article of the news app tha is just test one description of the single article of the news app tha is just test one",
+                style: TextStyle(color: Colors.grey, fontSize: 12, height: 1.4),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/screens/home_tabs/populer.dart
+++ lib/screens/home_tabs/populer.dart
@@ -1,17 +1,74 @@
 import 'package:flutter/material.dart';
 
-
-
 class Populer extends StatefulWidget {
   @override
   _PopulerState createState() => _PopulerState();
 }
 
 class _PopulerState extends State<Populer> {
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey,
+
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return  Padding(
+          padding: const EdgeInsets.only(
+              top: 8.0, left: 8.0, right: 8.0),
+          child: _SingleRowArticleTopStories(
+              'The World Global Warmin Annual Summit ',
+              'Michael Adams',
+              '15 min'),
+        );
+      },
+      itemCount:40,
+    );
+  }
+
+  Widget _SingleRowArticleTopStories(String _Title, _auther, _time) {
+    return Row(
+      children: <Widget>[
+        Image(
+          width: MediaQuery.of(context).size.width * 0.35,
+          height: MediaQuery.of(context).size.height * 0.15,
+          image: ExactAssetImage('assets/images/bg.png'),
+          fit: BoxFit.cover,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  _Title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  maxLines: 2,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(_auther),
+                    SizedBox(
+                      height: 52,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.access_time),
+                        Text(_time),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
+
+
